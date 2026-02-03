407572dc33169d80b86f0c0934e47c1aec119ffd
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-02-03 21:14:30 +0200

    There Was Nothing to DO

---
+++ There Was Nothing to Do/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <utility>
+#include <array>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <unordered_map>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    if(n%2==0){
+        n--;
+    }
+    cout<<n*n;
+
+    return 0;
+}
+
