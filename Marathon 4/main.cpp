1544020e897537c75014483db879cabb7353de1e
Hazem Elgindy <therealhazemelgindy@gmail.com>
2026-03-03 00:59:34 +0200

    Marathon 4

---
+++ Marathon 4/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <utility>
+#include <array>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <unordered_map>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    cout<<76;
+
+    return 0;
+}
+
